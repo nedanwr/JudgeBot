c1ea7509b93620bd074e2e3acc6b31f5b7f60d7b
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-07 14:54:44 +0500

    Feat: once ready, call start uptime counter function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { JudgeBot } from "./struct/Client";
+import { startUptimeCounter } from "./uptime";
 const config = require("../config.json");
 
 const client: JudgeBot = new JudgeBot({
     token: config.token,
     prefix: config.prefix,
+});
+
+// Start uptime counter once ready
+client.once("ready", () => {
+    startUptimeCounter();
 });
