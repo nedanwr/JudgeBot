1023be7a8333a2ac5b71dd6323098d744dab0a1c
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-07 15:47:37 +0500

    Feat: clean up

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { JudgeBot } from "./struct/Client";
 import { startUptimeCounter } from "./uptime";
 import { loadEvents } from "./utils/loadEvents";
-const config = require("../config.json");
 
-const client: JudgeBot = new JudgeBot({
-    token: config.token,
-    prefix: config.prefix,
-});
-
-// Load Events
-loadEvents(client);
+const client: JudgeBot = new JudgeBot();
 
 // Start uptime counter once ready
 client.once("ready", () => {
     startUptimeCounter();
 });
+
+// TODO: use top level await if we switch to ESM
+void client
+    .login(client.config.botSettings().token)
+    .then(async () => {
+        await loadEvents(client);
+    });
