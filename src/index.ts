359707ac4387646031a57cd28cb672aabd0ac636
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-08-07 15:01:09 +0500

    Feat: call the load events function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { JudgeBot } from "./struct/Client";
 import { startUptimeCounter } from "./uptime";
+import { loadEvents } from "./utils/loadEvents";
 const config = require("../config.json");
 
 const client: JudgeBot = new JudgeBot({
     token: config.token,
     prefix: config.prefix,
 });
+
+// Load Events
+loadEvents(client);
 
 // Start uptime counter once ready
 client.once("ready", () => {
